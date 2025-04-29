2207f515721c3cf3ab2f50662ead1b33165205cd
Taylor Jadin <jadin.taylor@gmail.com>
2025-04-29 10:54:45 -0500

    Update dbbench.sh

--- dbbench.sh
+++ dbbench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo 'Preparing test data...' &&
-sysbench oltp_read_write --tables=10 --table-size=100000 --mysql-db=test_db --mysql-user=testuser --mysql-password=testpass --mysql-host=localhost prepare &&
-echo 'Running benchmark...' &&
-sysbench oltp_read_write --tables=10 --table-size=100000 --mysql-db=test_db --mysql-user=testuser --mysql-password=testpass --mysql-host=localhost --time=60 --threads=4 --report-interval=10 run &&
-echo 'Cleaning up test data...' &&
+echo 'Preparing test data...'
+sysbench oltp_read_write --tables=10 --table-size=100000 --mysql-db=test_db --mysql-user=testuser --mysql-password=testpass --mysql-host=localhost prepare
+echo 'Running benchmark...'
+sysbench oltp_read_write --tables=10 --table-size=100000 --mysql-db=test_db --mysql-user=testuser --mysql-password=testpass --mysql-host=localhost --time=60 --threads=4 --report-interval=10 run
+echo 'Cleaning up test data...'
 sysbench oltp_read_write --tables=10 --table-size=100000 --mysql-db=test_db --mysql-user=testuser --mysql-password=testpass --mysql-host=localhost cleanup
